e44606ec11ad1ac2f57640f5f4d7c4c2717a1de8
yeswanthy994-crypto <yeswanthy994@gmail.com>
2026-03-12 09:10:09 +0530

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './LAB-6/' // Set base to current directory for correct asset paths
+  base: '/LAB-6/' // Set base to current directory for correct asset paths
 })
