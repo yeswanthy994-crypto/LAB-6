08d7b09cb6c430bc69986359385b424feb342b25
yeswanthy994-crypto <yeswanthy994@gmail.com>
2026-03-12 08:19:02 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './LAB-6/' // Set base to current directory for correct asset paths
 })
